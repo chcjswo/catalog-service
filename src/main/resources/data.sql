bdcba3292f451483da04f79a7b44b1599b94932d
tony <chcjswo@gmail.com>
2021-07-27 00:44:56 +0900

    쿼리문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ insert into catalog (product_id, product_name, stock, unit_price)
 values
     ('CATALOG-001', 'chcjswo', 100, 1500),
     ('CATALOG-002', 'tony', 200, 3500),
-    ('CATALOG-003', 'mc.jeon', 300, 5500),
+    ('CATALOG-003', 'mc.jeon', 300, 5500);
